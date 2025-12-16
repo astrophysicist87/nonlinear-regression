dd04f0a3d4d3acd46a21d7f99c7fe00a2e4c1450
Christopher Plumberg <plumberg.1@osu.edu>
2025-12-16 09:52:33 -0800

    Add options for how to enter data

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,15 +11,28 @@ st.set_page_config(page_title="Movies dataset (Plumberg)", page_icon="🎬")
 st.title("🎬 Movies dataset (Plumberg 2)")
 st.write(
     """
-    This app visualizes data from [The Movie Database (TMDB)](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata).
-    It shows which movie genre performed best at the box office over the years. Just 
-    click on the widgets below to explore!
+    This app performs non-linear regression for data collected from an LRC circuit experiment and visualizes the resulting best-fit curve in comparison to the collected data points.
     """
 )
 
-st.text_input("Your name", key="name")
-st.write("Welcome, ", st.session_state.name, "!")
+#st.text_input("Your name", key="name")
+#st.write("Welcome, ", st.session_state.name, "!")
 
+
+option_map = {
+    0: "Upload data from file",
+    1: "Enter data manually",
+}
+selection = st.pills(
+    "Tool",
+    options=option_map.keys(),
+    format_func=lambda option: option_map[option],
+    selection_mode="single",
+)
+st.write(
+    "Your selected option: "
+    f"{None if selection is None else option_map[selection]}"
+)
 
 # Load the data from a CSV. We're caching this so it doesn't reload every time the app
 # reruns (e.g. if the user interacts with the widgets).
@@ -29,49 +42,6 @@ def load_data(uploaded_file):
     #df = pd.read_excel(uploaded_file)
     return df
 
-
-#df = load_data()
-
-# Show a multiselect widget with the genres using `st.multiselect`.
-#genres = st.multiselect(
-#    "Genres",
-#    df.genre.unique(),
-#    ["Action", "Adventure", "Biography", "Comedy", "Drama", "Horror"],
-#)
-
-# Show a slider widget with the years using `st.slider`.
-#years = st.slider("Years", 1986, 2006, (2000, 2016))
-
-# Filter the dataframe based on the widget input and reshape it.
-#df_filtered = df[(df["genre"].isin(genres)) & (df["year"].between(years[0], years[1]))]
-#df_reshaped = df_filtered.pivot_table(
-#    index="year", columns="genre", values="gross", aggfunc="sum", fill_value=0
-#)
-#df_reshaped = df_reshaped.sort_values(by="year", ascending=False)
-
-
-# Display the data as a table using `st.dataframe`.
-#st.dataframe(
-#    df_reshaped,
-#    use_container_width=True,
-#    column_config={"year": st.column_config.TextColumn("Year")},
-#)
-
-# Display the data as an Altair chart using `st.altair_chart`.
-#df_chart = pd.melt(
-#    df_reshaped.reset_index(), id_vars="year", var_name="genre", value_name="gross"
-#)
-#chart = (
-#    alt.Chart(df_chart)
-#    .mark_line()
-#    .encode(
-#        x=alt.X("year:N", title="Year"),
-#        y=alt.Y("gross:Q", title="Gross earnings ($)"),
-#        color="genre:N",
-#    )
-#    .properties(height=320)
-#)
-#st.altair_chart(chart, use_container_width=True)
 
 
 uploaded_file = st.file_uploader("Choose a file")
@@ -97,12 +67,8 @@ edited_df = st.data_editor(df2, num_rows="dynamic",\
         ),
     },)
 
-#favorite_command = edited_df.loc[edited_df["rating"].idxmax()]["command"]
-#st.markdown(f"Your favorite command is **{favorite_command}** 🎈")
 
 fig = px.line(edited_df, x='x', y='y')
 
 st.plotly_chart(fig)
 
-
-#st.write("test = ",edited_df.cumsum())
