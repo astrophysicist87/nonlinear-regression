55c6728105cc9e7342fe6157b697c3e998ac6c91
Christopher Plumberg <astrophysicist87@gmail.com>
2025-12-17 12:59:34 -0800

    Alternate

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,7 +130,7 @@ if selection == 0:
                 df_data = pd.DataFrame({'t': xData, 'f(t)': yData, 'Type': 'Data Points'})
                 
                 # Print r^2
-                st.write('''r^2 = ''', get_r_squared(yData, fit_function(xData, *fitted_params)))
+                st.write(r'''r^2 = ''', get_r_squared(yData, fit_function(xData, *fitted_params)))
             
                 # Create the scatter plot for data and line plot for the fit
                 fig = px.scatter(df_data, x='t', y='f(t)', title="Data Points and Fitted Curve (File Upload)")
@@ -215,7 +215,7 @@ elif selection == 1:
             df_data = pd.DataFrame({'t': xData, 'f(t)': yData, 'Type': 'Data Points'})
             
             # Print r^2
-            st.write('''r^2 = ''', get_r_squared(yData, fit_function(xData, *fitted_params)))
+            st.write(r'''r^2 = ''', get_r_squared(yData, fit_function(xData, *fitted_params)))
             
             # Create the scatter plot for data and line plot for the fit
             fig = px.scatter(df_data, x='t', y='f(t)', title="Data Points and Fitted Curve (Manual Input)")
